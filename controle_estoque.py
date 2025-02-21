0f5f6e5d41279bdecdc0e3adf99b74603f0a6f96
wnomura <willian.nomura@hotmail.com>
2025-02-21 15:33:39 +0000

    Adding Ex2 to Ex6

---
+++ controle_estoque.py
@@ -0,0 +1,59 @@
+print('Controle de estoque\n')
+
+estoque = []
+
+def cadastrarProduto(nome, quantidade, preco):
+    produto = {'nome': nome, 'quantidade': quantidade, 'preco': preco}
+    estoque.append(produto)
+
+while True:
+    print('1: Cadastrar Produto\n'
+          '2: Adicionar Produto\n'
+          '3: Remover Produto\n'
+          '4: Visualizar Estoque\n'
+          '5: Sair\n')
+
+    try:
+        acao = int(input('Escolha a opção desejada: '))
+
+        if acao == 1:
+            nome = input('Qual é o nome do produto? ')
+            quantidade = int(input('Qual é a quantidade? '))
+            preco = float(input('Qual é o preço de venda? € '))
+            cadastrarProduto(nome, quantidade, preco)
+            print(f'\nO produto {nome} foi cadastrado com sucesso!\n')
+
+        elif acao == 2:
+            nomeProduto = input('\nQue produto quer adicionar ao estoque? ')
+            produtoEncontrado = False
+            for produto in estoque:
+                if produto['nome'].lower() == nomeProduto.lower():
+                    quantidadeAdicional = int(input('Quantas unidades deseja adicionar? '))
+                    produto['quantidade'] += quantidadeAdicional
+                    print(f"Quantidade de {produto['nome']} atualizada! Agora temos {produto['quantidade']} unidadades no estoque.\n")
+
+        elif acao == 3:
+            nomeProduto = input('\nQue produto quer remover do estoque? ')
+            produtoEncontrado = False
+            for produto in estoque:
+                if produto['nome'].lower() == nomeProduto.lower():
+                    quantidadeRemover = int(input('Quantas unidades deseja remover? '))
+                    produto['quantidade'] -= quantidadeRemover
+                    print(f"Quantidade de {produto['nome']} atualizada! Agora temos {produto['quantidade']} unidadades no estoque.\n")
+
+        elif acao == 4:
+            if not estoque:
+                print('\nO estoque está vazio.\n')
+            else:
+                print('\nEstoque atual: ')
+                for produto in estoque:
+                    print(f"\nNome: {produto['nome']} | Quantidade: {produto['quantidade']} | Preço: € {produto['preco']:.2f}\n")
+
+        elif acao == 5:
+            print('O programa será encerrado.')
+            break
+
+        else:
+            print('\nOpção inválida, tente novamente.\n')
+    except ValueError:
+        print('Entrada inválida! Por favor informe um número válido.\n')
